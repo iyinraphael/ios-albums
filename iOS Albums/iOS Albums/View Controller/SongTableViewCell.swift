608661763966ea6ac2a56e190d32945cd6e9e078
iyinraphael <iyinraphael@gmail.com>
2019-01-30 05:58:07 -0800

    finished with song TableViewCell

--- iOS Albums/iOS Albums/View Controller/SongTableViewCell.swift
+++ iOS Albums/iOS Albums/View Controller/SongTableViewCell.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class SongTableViewCell: UITableViewCell {
 
+    weak var delegate: SongTableViewCellDelegate?
+    var song: Songs?
+    
+    func updateView(){
+        if let song = song {
+            titleTextfield.text = song.name
+            durationTextField.text = song.duration
+            button.alpha = 0.0
+        }
+    }
+    
+    override func prepareForReuse() {
+        titleTextfield.text = " "
+        durationTextField.text = " "
+        button.alpha = 1.0
+    }
+    
+    
+    
+    @IBOutlet weak var button: UIButton!
     @IBOutlet weak var titleTextfield: UITextField!
     @IBOutlet weak var durationTextField: UITextField!
     
     @IBAction func addSongButton(_ sender: Any) {
+        guard let title = titleTextfield.text,
+        let duration = durationTextField.text else {return}
+        
+        delegate?.addSong(with: title, duration: duration)
     }
+}
+
+
+protocol SongTableViewCellDelegate: class {
+    
+    func addSong(with title: String, duration: String)
+    
 }
